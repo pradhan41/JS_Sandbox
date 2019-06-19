d8a0e44759d1d7d08c3bad6493dae487156c8b0c
pradhan41 <35980364+pradhan41@users.noreply.github.com>
2019-06-19 13:45:59 -0400

    Create dataTypes.js

---
+++ dataTypes.js
@@ -0,0 +1,52 @@
+console.log('Hello world !');
+//String
+const name = 'Gangaram Pradhan';
+console.log(typeof name);
+
+//numeric
+const age =37;
+console.log(typeof age);
+
+//Boolean
+const hasKids =true;
+console.log(typeof hasKids);
+
+//Null
+
+const car = null;
+console.log (typeof car);
+
+//Undefined
+
+let test;
+
+console.log(typeof test);
+
+const sym = Symbol();
+
+const hobbies = ['reading', 'hiking','swimming', 'walking'];
+
+console.log(typeof hobbies);
+
+
+//Objet literal
+const address = {
+    Steet:'Main St',
+    City: 'Boston',
+    State:'MA',
+    Zipcode: '23456'
+}
+    console.log(typeof address);
+    console.log(address);
+
+const today = new Date();
+console.log(today);
+console.log(typeof today);
+
+
+
+
+
+
+
+
