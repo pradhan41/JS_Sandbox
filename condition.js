60d050b6f89644da4b29035fa10aa25261c24ca8
Gangaram <gangaram.pradhan@gmail.com>
2019-07-02 00:01:02 -0400

    condition added

---
+++ condition.js
@@ -0,0 +1,87 @@
+const id =100;
+
+//Equal to
+
+/* if(id == 200){
+    console.log('Coorect');
+}else{
+    console.log('incorrect');
+}
+
+
+Not equal to
+
+if(id != 100){
+    console.log('Coorect');
+}else{
+    console.log('incorrect');
+}
+
+Equal to value and type
+
+if (id === 100){
+    console.log('Corect');a
+}else {a
+    console.log('Incorrect');
+
+}
+  */
+
+const color ='black';
+
+/* if(color === 'red'){
+    console.log('color is red');
+} else if(color==='blue'){
+    console.log('color is not red');
+} else if (color=='yellow'){
+    console.log('color is yellow');
+} else {
+    console.log('color is black');
+} */
+
+const name = 'Manas';
+const age = '3';
+
+// AND &&
+/* if (age > 0 && age< 12){
+    console.log(`${name} is a child`);
+} else if(age >13 && age <= 19){
+    console.log(`${name} is an teenager`);
+} else {
+    console.log(`${name} is an adult`);
+
+}
+ */
+// OR ||
+/* if(age <16 || age > 65){
+    console.log(`${name} can not run the race`);
+} else {
+    console.log(`${name} is registered for the race`);
+}
+
+//Ternanry operator
+
+console.log(id ===100 ? 'correct' : 'incoorect');
+
+
+ */
+
+ //switch 
+
+ /* console.log(id === 101 ? 'incorrect' : 'correct');
+
+ switch(color){
+     case 'red':
+         console.log('color is red');
+         break;
+         case 'blue':
+         console.log('color is blue');
+         break;
+         default:
+             console.log('color is not blue or red');
+
+ } */
+
+
+let today = new Date();
+let dd = today;
